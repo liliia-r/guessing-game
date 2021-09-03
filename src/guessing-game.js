323962704e951b0f81571f0f0c898e9d701b205e
liliia-r <54743392+liliia-r@users.noreply.github.com>
2021-09-03 21:24:12 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.middle = Math.round((this.min + this.max) / 2);
+    return this.middle;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.middle;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.middle;
+  }
 }
 
 module.exports = GuessingGame;
